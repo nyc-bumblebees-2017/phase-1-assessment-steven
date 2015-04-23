93f1b0c81e837ad032fd61190bbdfa391dbaa6b9
Torey Hickman <torey@toreyhickman.com>
2015-04-23 14:02:57 -0500

    Carilfy SockDrawer method names

--- part-2/spec/sock_drawer_spec.rb
+++ part-2/spec/sock_drawer_spec.rb
@@ -20,16 +20,16 @@ describe SockDrawer do
     expect(drawer.matcher).to eq matcher
   end
 
-  describe "#match_for" do
+  describe "#supply_match_for" do
     let(:right_red_sock) { Sock.new(brand: "Nike", style: "quarter", color: "red") }
 
     context "when it has a matching sock" do
       it "returns a matching sock" do
-        expect(drawer.match_for right_red_sock).to eq left_red_sock
+        expect(drawer.supply_match_for right_red_sock).to eq left_red_sock
       end
 
       it "removes the matching sock from its collection of socks" do
-        drawer.match_for(right_red_sock)
+        drawer.supply_match_for(right_red_sock)
         expect(drawer.socks).to_not include left_red_sock
       end
     end
@@ -38,19 +38,19 @@ describe SockDrawer do
       it "returns nothing" do
         left_yellow_sock = Sock.new(brand: "Nike", style: "quarter", color: "yellow")
 
-        expect(drawer.match_for left_yellow_sock).to be_nil
+        expect(drawer.supply_match_for left_yellow_sock).to be_nil
       end
     end
   end
 
-  describe "#random_pair_of_socks", { pending: true } do
+  describe "#supply_random_pair_of_socks", { pending: true } do
     context "when it has one or more pairs of matching socks" do
       it "returns one of its pairs of socks" do
-        expect(drawer.random_pair_of_socks).to match_array [left_white_sock, right_white_sock]
+        expect(drawer.supply_random_pair_of_socks).to match_array [left_white_sock, right_white_sock]
       end
 
       it "removes paired socks from its collection of socks" do
-        drawer.random_pair_of_socks
+        drawer.supply_random_pair_of_socks
 
         [left_white_sock, right_white_sock].each do |paired_sock|
           expect(drawer.socks).to_not include paired_sock
@@ -63,7 +63,7 @@ describe SockDrawer do
         non_matching_socks = [left_white_sock, left_red_sock]
         drawer_with_no_matching_socks = SockDrawer.new(socks: non_matching_socks, matcher: matcher)
 
-        expect(drawer_with_no_matching_socks.random_pair_of_socks).to be_empty
+        expect(drawer_with_no_matching_socks.supply_random_pair_of_socks).to be_empty
       end
     end
   end
