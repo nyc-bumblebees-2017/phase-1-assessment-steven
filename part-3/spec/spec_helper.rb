3f833c98e500d46fb85d80c31c934137d0cd5817
Chris DiMartino <vwchris@gmail.com>
2015-05-04 17:00:38 -0400

    Adding calls to autoload files to deal with order dependency when using require

--- part-3/spec/spec_helper.rb
+++ part-3/spec/spec_helper.rb
@@ -2,6 +2,6 @@ require 'active_support/inflector'
 
 path_to_part_3_folder = File.expand_path('../..', __FILE__)
 Dir["#{path_to_part_3_folder}/*.rb"].each do |file|
-  klass = File.basename(file).split('.').first.classify
+  klass = File.basename(file).gsub(/(?:^|_)(.)/) { |x| x.upcase }.gsub(/_/, '').sub(/\.rb\Z/, '')
   autoload klass.to_sym, file
 end
