53fb81d3be1ee90d5f7a763f2928faf5b4046c77
Steven G. Harms <steven@devbootcamp.com>
2016-01-29 14:21:30 -0500

    Rename RSpec sock methods
    Addresses #92 per @dpb-dbc.

--- part-2/spec/sock_drawer_spec.rb
+++ part-2/spec/sock_drawer_spec.rb
@@ -1,11 +1,11 @@
 require_relative "spec_helper"
 
 describe SockDrawer do
-  let(:socks) { [left_white_sock, left_black_sock, left_red_sock, right_white_sock] }
-  let(:left_white_sock)  { Sock.new(brand: "Nike", style: "quarter", color: "white") }
-  let(:right_white_sock) { Sock.new(brand: "Nike", style: "quarter", color: "white") }
-  let(:left_red_sock)    { Sock.new(brand: "Nike", style: "quarter", color: "red") }
-  let(:left_black_sock)    { Sock.new(brand: "Nike", style: "quarter", color: "black") }
+  let(:socks) { [first_white_sock, first_black_sock, first_red_sock, second_white_sock] }
+  let(:first_white_sock)  { Sock.new(brand: "Nike", style: "quarter", color: "white") }
+  let(:second_white_sock) { Sock.new(brand: "Nike", style: "quarter", color: "white") }
+  let(:first_red_sock)    { Sock.new(brand: "Nike", style: "quarter", color: "red") }
+  let(:first_black_sock)    { Sock.new(brand: "Nike", style: "quarter", color: "black") }
 
   let(:matcher) { SockMatcher.new }
 
@@ -21,24 +21,24 @@ describe SockDrawer do
   end
 
   describe "#supply_match_for", { supply_match_for: true } do
-    let(:right_red_sock) { Sock.new(brand: "Nike", style: "quarter", color: "red") }
+    let(:second_red_sock) { Sock.new(brand: "Nike", style: "quarter", color: "red") }
 
     context "when it has a matching sock" do
       it "returns a matching sock" do
-        expect(drawer.supply_match_for right_red_sock).to eq left_red_sock
+        expect(drawer.supply_match_for second_red_sock).to eq first_red_sock
       end
 
       it "removes the matching sock from its collection of socks" do
-        drawer.supply_match_for(right_red_sock)
-        expect(drawer.socks).to_not include left_red_sock
+        drawer.supply_match_for(second_red_sock)
+        expect(drawer.socks).to_not include first_red_sock
       end
     end
 
     context "when it has no matching socks" do
       it "returns nothing" do
-        left_yellow_sock = Sock.new(brand: "Nike", style: "quarter", color: "yellow")
+        first_yellow_sock = Sock.new(brand: "Nike", style: "quarter", color: "yellow")
 
-        expect(drawer.supply_match_for left_yellow_sock).to be_nil
+        expect(drawer.supply_match_for first_yellow_sock).to be_nil
       end
     end
   end
@@ -46,13 +46,13 @@ describe SockDrawer do
   describe "#supply_random_pair_of_socks", { supply_random_pair_of_socks: true } do
     context "when it has one or more pairs of matching socks" do
       it "returns one of its pairs of socks" do
-        expect(drawer.supply_random_pair_of_socks).to match_array [left_white_sock, right_white_sock]
+        expect(drawer.supply_random_pair_of_socks).to match_array [first_white_sock, second_white_sock]
       end
 
       it "removes paired socks from its collection of socks" do
         drawer.supply_random_pair_of_socks
 
-        [left_white_sock, right_white_sock].each do |paired_sock|
+        [first_white_sock, second_white_sock].each do |paired_sock|
           expect(drawer.socks).to_not include paired_sock
         end
       end
@@ -60,7 +60,7 @@ describe SockDrawer do
 
     context "when it has no pairs of matching socks" do
       it "returns a collection with no socks" do
-        non_matching_socks = [left_white_sock, left_red_sock]
+        non_matching_socks = [first_white_sock, first_red_sock]
         drawer_with_no_matching_socks = SockDrawer.new(socks: non_matching_socks, matcher: matcher)
 
         expect(drawer_with_no_matching_socks.supply_random_pair_of_socks).to be_empty
