b7ae1be352724823bd558c28972f77d72e1b7ae6
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-04-13 09:30:29 -0400

    create class Roadtrip

--- part-1/road_trip.rb
+++ part-1/road_trip.rb
@@ -1,2 +1,22 @@
 class RoadTrip
+  attr_accessor :name
+  attr_reader :segments
+  def initialize(args = {})
+    @name = args[:name]
+    @segments = args[:segments]
+  end
+
+  def add_segment(new_segment)
+    segments << new_segment
+  end
+
+  def total_miles
+    total = segments.map { |segment| segment.instance_variable_get(:@miles) }.inject {|sum, miles| sum + miles}
+    total
+  end
+
+  def average_miles_per_segment
+    avg = segments.map { |segment| segment.instance_variable_get(:@miles) }.inject(:+) / segments.size.to_f
+    avg 
+  end
 end
