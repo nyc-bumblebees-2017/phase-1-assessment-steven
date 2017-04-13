c0e43a5318cdbf28c9c77a911aa74322bef8c22c
theasteve <steven.aguilar@stu.bmcc.cuny.edu>
2017-04-13 12:27:47 -0400

    update SockDrower classs

--- part-2/sock_drawer.rb
+++ part-2/sock_drawer.rb
@@ -1,8 +1,18 @@
+
+
 class SockDrawer
   attr_reader :socks, :matcher
 
   def initialize(args = {})
     @socks = args.fetch(:socks) { Array.new }
     @matcher = args.fetch(:matcher)
+  end
+
+  def supply_match_for(my_sock)
+   socks.select {|sock| match?(my_sock, sock)}
+  end
+
+  def supply_one_pair_of_socks
+    socks.sample(2).delete
   end
 end
